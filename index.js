b8b5cf34c7d2352906e09a286ab568aff661f65a
Radoslav Ivanov <102248172+BeepBoopItsJoop@users.noreply.github.com>
2022-09-19 00:23:39 +0200

    Added Functionality

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-// const buttonSet = document.querySelectorAll('')
+const buttonSet = document.querySelectorAll('.rating-button')
 const submitButton = document.querySelector('#submit')
 const ratingPage = document.querySelector('#rating')
 const thankYouPage = document.querySelector('#thank-you')
 
-submitButton.addEventListener('submit', (e) => {
-    // e.preventDefault()
-    console.log('hi')
-    // ratingPage.classList.add('hidden')
-    // thankYouPage.classList.toggle('hidden')
+const resultText = document.querySelector('#result-text')
+
+let selectedOption
+buttonSet.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        selectedOption = e.target.dataset.index
+        resultText.textContent = selectedOption
+    })
+})
+
+submitButton.addEventListener('click', (e) => {
+    if (!selectedOption) return
+    ratingPage.classList.add('hidden')
+    thankYouPage.classList.toggle('hidden')
 })

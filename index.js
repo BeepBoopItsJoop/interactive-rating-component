44118ed2a07826e27ea6bdb3aca684ff4ce844bd
Radoslav Ivanov <102248172+BeepBoopItsJoop@users.noreply.github.com>
2022-09-19 00:02:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// const buttonSet = document.querySelectorAll('')
+const submitButton = document.querySelector('#submit')
+const ratingPage = document.querySelector('#rating')
+const thankYouPage = document.querySelector('#thank-you')
+
+submitButton.addEventListener('submit', (e) => {
+    // e.preventDefault()
+    console.log('hi')
+    // ratingPage.classList.add('hidden')
+    // thankYouPage.classList.toggle('hidden')
+})
